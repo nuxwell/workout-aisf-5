60bac4aeb02677042a81611931cd28edb298c48c
nuxwell <mujeebrpp@nuxwell.com>
2025-06-29 14:25:51 +0530

    Updated package-lock.json

--- next.config.js
+++ next.config.js
@@ -4,7 +4,12 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true },
+  images: { 
+    unoptimized: true 
+  },
+  experimental: {
+    optimizePackageImports: ['lucide-react']
+  }
 };
 
 module.exports = nextConfig;
